346f03aa657f8a82afe1262ac2354e345b87f3d2
alevko55555 <alevko55555@gmail.com>
2019-11-22 10:40:46 +0300

    Scripted auto-commit on change (2019-11-22 10:40:46) by gitwatch.sh

--- lab/src/main/java/lab5/Main.java
+++ lab/src/main/java/lab5/Main.java
@@ -1,6 +1,17 @@
 package lab5;
 
+import akka.NotUsed;
+import akka.actor.ActorSystem;
+import akka.http.javadsl.ConnectHttp;
+import akka.http.javadsl.Http;
+import akka.http.javadsl.ServerBinding;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
+import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
+
 import java.io.IOException;
+import java.util.concurrent.CompletionStage;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -9,19 +20,16 @@ public class Main {
         final Http http = Http.get(system);
         final ActorMaterializer materializer =
                 ActorMaterializer.create(system);
-        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = <вызов
-        метода которому передаем Http, ActorSystem и ActorMaterializer>;
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = ; //<вызов метода которому передаем Http, ActorSystem и ActorMaterializer>;
         final CompletionStage<ServerBinding> binding = http.bindAndHandle(
                 routeFlow,
-                ConnectHttp.toHost("localhost", 8080),
+                ConnectHttp.toHost("localhost", PORT),
                 materializer
         );
-        System.out.println("Server online at http://localhost:8080/\nPress
-                RETURN to stop...");
+        System.out.println("Server online at http://localhost:8080/\nPress RETURN to stop...");
         System.in.read();
         binding
                 .thenCompose(ServerBinding::unbind)
-                .thenAccept(unbound -> system.terminate()); // and shutdown
-        when done
+                .thenAccept(unbound -> system.terminate()); // and shutdown when done
     }
 }
