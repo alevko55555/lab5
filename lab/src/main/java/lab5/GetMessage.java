b15865c6cd0cd420a1979d2c05470d2414bc8108
alevko55555 <alevko55555@gmail.com>
2019-11-23 12:38:41 +0300

    Scripted auto-commit on change (2019-11-23 12:38:41) by gitwatch.sh

--- lab/src/main/java/lab5/GetMessage.java
+++ lab/src/main/java/lab5/GetMessage.java
@@ -1,4 +1,21 @@
 package lab5;
 
+import javafx.util.Pair;
+
 public class GetMessage {
+    Pair<String, Integer> msgPair;
+
+    public GetMessage(Pair<String, Integer> pair) {
+        this.msgPair = pair;
+    }
+
+    public Pair<String, Integer> getMsgPair(){
+        return msgPair;
+    }
+    public String getUrl() {
+        return  msgPair.getKey();
+    }
+    public Integer getNum() {
+        return  msgPair.getValue();
+    }
 }
