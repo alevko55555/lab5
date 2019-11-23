a1a6b87d915b4d1562b5cfe5f43534959161ea51
alevko55555 <alevko55555@gmail.com>
2019-11-23 12:59:27 +0300

    Scripted auto-commit on change (2019-11-23 12:59:27) by gitwatch.sh

--- lab/src/main/java/lab5/FlowWorkNode.java
+++ lab/src/main/java/lab5/FlowWorkNode.java
@@ -1,4 +1,17 @@
 package lab5;
 
+import akka.actor.ActorRef;
+import akka.stream.ActorMaterializer;
+import org.asynchttpclient.AsyncHttpClient;
+
 public class FlowWorkNode {
+    private final AsyncHttpClient asyncHttpClient;
+    private final ActorRef storage;
+    private final ActorMaterializer actorMaterializer;
+
+    public FlowWorkNode(AsyncHttpClient asyncHttpClient, ActorRef storage, ActorMaterializer actorMaterializer) {
+        this.asyncHttpClient = asyncHttpClient;
+        this.storage = storage;
+        this.actorMaterializer = actorMaterializer;
+    }
 }
