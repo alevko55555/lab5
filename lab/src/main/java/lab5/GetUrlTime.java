d815344681a01f8806fc37b87594c6555ea21eac
alevko55555 <alevko55555@gmail.com>
2019-11-23 12:44:04 +0300

    Scripted auto-commit on change (2019-11-23 12:44:04) by gitwatch.sh

--- lab/src/main/java/lab5/GetUrlTime.java
+++ lab/src/main/java/lab5/GetUrlTime.java
@@ -1,4 +1,21 @@
 package lab5;
 
+import javafx.util.Pair;
+
 public class GetUrlTime {
+    private Pair<String, Integer> msg;
+
+    public GetUrlTime(Pair<String, Integer> msg) {
+        this.msg = msg;
+    }
+
+    public String getUrl(){
+        return msg.getKey();
+    }
+    public Integer getNum(){
+        return  msg.getValue();
+    }
+    public Integer getTime(){
+        //return msg.getValue()
+    }
 }
