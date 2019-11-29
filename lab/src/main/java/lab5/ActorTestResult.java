fbe5099a4527e623c3c53a410f02bd4494642d4b
alevko55555 <alevko55555@gmail.com>
2019-11-30 01:33:43 +0300

    Scripted auto-commit on change (2019-11-30 01:33:43) by gitwatch.sh

--- lab/src/main/java/lab5/ActorTestResult.java
+++ lab/src/main/java/lab5/ActorTestResult.java
@@ -2,8 +2,13 @@ package lab5;
 
 import akka.actor.AbstractActor;
 
+import java.util.HashMap;
+
 public class ActorTestResult extends AbstractActor {
-    private final HashMap<String, Map>
+    private HashMap<GetTest, Integer> storage;
+    public void TestStorage() {
+        this.storage = new HashMap<>();
+    }
 
     @Override
     public Receive createReceive() {
