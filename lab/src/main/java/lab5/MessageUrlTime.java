09b2bb8ae8a03abb5807d8055b748299c646e59a
alevko55555 <alevko55555@gmail.com>
2019-11-30 01:40:45 +0300

    Scripted auto-commit on change (2019-11-30 01:40:45) by gitwatch.sh

--- lab/src/main/java/lab5/MessageUrlTime.java
+++ lab/src/main/java/lab5/MessageUrlTime.java
@@ -1,4 +1,13 @@
 package lab5;
 
 public class MessageUrlTime {
+    private final GetUrlTime result;
+
+    public MessageUrlTime(GetUrlTime result) {
+        this.result = result;
+    }
+
+    public Optional<GetUrlTime> getUrlTimeOptional() {
+        if (result.)
+    }
 }
