dfd8638f89e428fab90825f17dfe835c332aecfd
alevko55555 <alevko55555@gmail.com>
2019-12-02 16:34:12 +0300

    Scripted auto-commit on change (2019-12-02 16:34:12) by gitwatch.sh

--- lab/src/main/java/lab5/ActorTestResult.java
+++ lab/src/main/java/lab5/ActorTestResult.java
@@ -4,23 +4,33 @@ import akka.actor.AbstractActor;
 import akka.actor.ActorRef;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class ActorTestResult extends AbstractActor {
-    private HashMap<GetTest, Long> storage = new HashMap<>();
+    private HashMap<String, Map<Integer, Integer>> storage = new HashMap<>();
 
     @Override
     public Receive createReceive() {
-        return receiveBuilder()
-                .match(GetTest.class,
-                        msg -> {
-                    //String url = msg.getUrl();
-                    //int count = msg.getNum();
-                    getSender().tell(
-                                new MessageUrlTime(new GetUrlTime(msg, storage.get(msg))),
-                                ActorRef.noSender()
-                        );})
-                .match(GetUrlTime.class,
-                        msg -> storage.put(msg.getTest(), msg.getNum()))
+        return receiveBuilder().create()
+                .match(GetTest.class, test -> {
+                    String url = test.getUrl();
+                    Integer count = test.getNum();
+                    if(storage.containsKey(url) && storage.get(url).containsKey(count)) {
+                        getSender().tell(storage.get(url).get(count), ActorRef.noSender());
+                    } else {
+                        getSender().tell(-1, ActorRef.noSender());
+                    }
+                })
+                .match(GetUrlTime.class, test -> {
+                    Map<Integer, Integer> store;
+                    if(storage.containsKey(test.getUrl())) {
+                        store = storage.get(test.getUrl());
+                    } else {
+                        store = new HashMap<>();
+                    }
+                    store.put(test.getCount(), test.getTime());
+                    store.put(test.getUrl(), store);
+                })
                 .build();
     }
 }
