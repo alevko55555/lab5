c010435979aa3af9ed1075000dda39d32f3bc4e3
alevko55555 <alevko55555@gmail.com>
2019-12-03 16:45:46 +0300

    Scripted auto-commit on change (2019-12-03 16:45:46) by gitwatch.sh

--- lab/src/main/java/lab5/FlowWorkNode.java
+++ lab/src/main/java/lab5/FlowWorkNode.java
@@ -17,7 +17,9 @@ import java.time.Duration;
 import java.util.Collections;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
+import java.util.concurrent.ExecutionException;
 
+import static java.lang.System.*;
 import static org.asynchttpclient.Dsl.asyncHttpClient;
 
 public class FlowWorkNode {
@@ -42,7 +44,7 @@ public class FlowWorkNode {
                     try {
                         countInt1 = Integer.parseInt(count);
                     } catch (NumberFormatException e) {
-                        System.out.println("Incorrect count value");
+                        out.println("Incorrect count value");
                         countInt1 = 0;
                     }
                     countInt = countInt1;
@@ -55,35 +57,45 @@ public class FlowWorkNode {
                             storage,
                             new GetTest(new javafx.util.Pair<>(data.first(), data.second())),
                             Duration.ofMillis(3000)
-                    ).thenCompose(
+                            ).thenCompose(
                             response -> {
-                                if ((int) response != -1) {
-                                    return CompletableFuture.completedFuture(response);
+                                if ((int) response != 1){
+                                    return CompletableFuture.completedFuture((int) response);
                                 }
-                                return Source.from(Collections.singletonList(pair)).toMat(Flow.<Pair<HttpRequest, Integer>>create().mapConcat(cp -> Collections.nCopies(cp.second(), cp.first())).mapAsync(
-                                        1, req2 -> CompletableFuture.supplyAsync(
-                                                () -> System.currentTimeMillis()
-                                        ).thenCompose(
-                                                start -> CompletableFuture.supplyAsync(
-                                                        () -> {
-                                                            CompletionStage<Long> responseServer = asyncHttpClient().prepareGet(req2.getUri().toString()).execute().toCompletableFuture().thenCompose(
-                                                                    answer -> CompletableFuture.completedFuture(System.currentTimeMillis() - start)
-                                                            );
-                                                            return responseServer;
-                                                        })))
-                                        .toMat(getFold(), Keep.right()), Keep.right()).run(actorMaterializer);
+                                return returnSource(pair);
                             }).thenCompose(
-                                    sum -> {
-                                        Patterns.ask(storage, new GetUrlTime(new javafx.util.Pair<String, javafx.util.Pair<Integer, Integer>>(data.first(), (new javafx.util.Pair<Integer, Integer>(data.second(), (int)sum)))), 5000);
-                                        Double avgTime = (double) sum / (double) countInt;
-                                        return CompletableFuture.completedFuture(HttpResponse.create().withEntity(ByteString.fromString(avgTime.toString())));
-                                    }
-                                    )
+                            sum -> {
+                                Patterns.ask(storage, new GetUrlTime(new javafx.util.Pair<String, javafx.util.Pair<Integer, Integer>>(data.first(), (new javafx.util.Pair<Integer, Integer>(data.second(), (int) sum)))), 5000);
+                                Double avgTime = (double) sum / (double) countInt;
+                                return CompletableFuture.completedFuture(HttpResponse.create().withEntity(ByteString.fromString(avgTime.toString())));
+                            }
+                            )
                     );
                     CompletionStage<HttpResponse> result = source.via(testSink).toMat(Sink.last(), Keep.right()).run(actorMaterializer);
                     return result.toCompletableFuture().get();
                 });
         return flow;
+    }
+
+
+    public CompletionStage<Integer> returnSource(Pair<HttpRequest, Integer> pair){
+        return Source.from(Collections.singletonList(pair)).toMat(
+                Flow.<Pair<HttpRequest, Integer>>create().mapConcat(p -> Collections.nCopies(p.second(), p.first()))
+                        .mapAsync(
+                                1,
+                                req2 -> CompletableFuture.completedFuture(currentTimeMillis())
+                                        .thenCompose(start ->
+                                                CompletableFuture.supplyAsync(
+                                                        () -> {
+                                                            CompletionStage<Long> whenResponse = asyncHttpClient
+                                                                    .prepareGet(req2.getUri().toString())
+                                                                    .execute().toCompletableFuture()
+                                                                    .thenCompose(
+                                                                            answer -> CompletableFuture.completedFuture(currentTimeMillis() - start));
+                                                            return whenResponse;
+                                                        }
+                                                ))
+                        ).toMat(getFold(), Keep.right()), Keep.right()).run(actorMaterializer);
     }
 
     public Sink<CompletionStage<Long>, CompletionStage<Integer>> getFold() {
--- lab/src/main/java/lab5/Main.java
+++ lab/src/main/java/lab5/Main.java
@@ -39,60 +39,7 @@ public class Main {
                 ActorMaterializer.create(system);
         final AsyncHttpClient asyncHttpClient = asyncHttpClient();
         final FlowWorkNode workNode = new FlowWorkNode(asyncHttpClient, system, materializer);
-        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = Flow.of(HttpRequest.class).map(
-                req -> {
-                    final int countInt;
-                    int countInt1;
-                    String url = req.getUri().query().get("testUrl").orElse("");
-                    String count = req.getUri().query().get("count").orElse("");
-                    try {
-                        countInt1 = Integer.parseInt(count);
-                    } catch (NumberFormatException e) {
-                        System.out.println("Incorrect count value");
-                        countInt1 = 0;
-                    }
-                    countInt = countInt1;
-                    Pair<String, Integer> data = new Pair<>(url, countInt);
-                    Source<Pair<String, Integer>, NotUsed> source = Source.from(Collections.singletonList(data));
-
-                    Flow<Pair<String, Integer>, HttpResponse, NotUsed> testSink = Flow.<Pair<String, Integer>>create().map(
-                            pair -> new Pair<>(HttpRequest.create().withUri(pair.first()), pair.second())
-                    ).mapAsync(1, pair -> Patterns.ask(
-                            storage,
-                            new GetTest(new javafx.util.Pair<>(data.first(), data.second())),
-                            Duration.ofMillis(3000)
-                            ).thenCompose(
-                            response -> {
-                                if ((int) response != -1) {
-                                    return CompletableFuture.completedFuture(response);
-                                }
-                                Sink<CompletionStage<Long>, CompletionStage<Integer>> fold = Sink.fold(0, (ac, el) -> {
-                                        int castEl = (int) (0 + el.toCompletableFuture().get());
-                                        return ac + castEl;
-                                    });
-                                return Source.from(Collections.singletonList(pair)).toMat(Flow.<Pair<HttpRequest, Integer>>create().mapConcat(cp -> Collections.nCopies(cp.second(), cp.first())).mapAsync(
-                                        1, req2 -> CompletableFuture.supplyAsync(
-                                                () -> System.currentTimeMillis()
-                                        ).thenCompose(
-                                                start -> CompletableFuture.supplyAsync(
-                                                        () -> {
-                                                            CompletionStage<Long> responseServer = asyncHttpClient().prepareGet(req2.getUri().toString()).execute().toCompletableFuture().thenCompose(
-                                                                    answer -> CompletableFuture.completedFuture(System.currentTimeMillis() - start)
-                                                            );
-                                                            return responseServer;
-                                                        })))
-                                        .toMat(fold, Keep.right()), Keep.right()).run(materializer);
-                            }).thenCompose(
-                            sum -> {
-                                Patterns.ask(storage, new GetUrlTime(new javafx.util.Pair<String, javafx.util.Pair<Integer, Integer>>(data.first(), (new javafx.util.Pair<Integer, Integer>(data.second(), (int)sum)))), 5000);
-                                Double avgTime = (double) sum / (double) countInt;
-                                return CompletableFuture.completedFuture(HttpResponse.create().withEntity(ByteString.fromString(avgTime.toString())));
-                            }
-                            )
-                    );
-                    CompletionStage<HttpResponse> result = source.via(testSink).toMat(Sink.last(), Keep.right()).run(materializer);
-                    return result.toCompletableFuture().get();
-                }); //<вызов метода которому передаем Http, ActorSystem и ActorMaterializer>;
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = new FlowWorkNode(asyncHttpClient, system, materializer).createRoute(); //<вызов метода которому передаем Http, ActorSystem и ActorMaterializer>;
         final CompletionStage<ServerBinding> binding = http.bindAndHandle(
                 routeFlow,
                 ConnectHttp.toHost("localhost", SERVER_PORT),
